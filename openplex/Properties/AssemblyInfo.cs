5a59984569deea29b1a6e46e3089a707cad0fca4
NV <bettercodes1@gmail.com>
2017-10-17 23:35:52 +0100

    added project to repo

--- openplex/Properties/AssemblyInfo.cs
+++ openplex/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("openplex")]
+[assembly: AssemblyTitle("OpenPlex")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("openplex")]
+[assembly: AssemblyCompany("NV")]
+[assembly: AssemblyProduct("OpenPlex")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
