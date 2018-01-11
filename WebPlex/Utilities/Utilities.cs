690b1b5a3f5a4b6e1802d880aec1b010f53bb5fd
NV <bettercodes1@gmail.com>
2018-01-11 23:17:40 +0000

    Added support for local sub directory files

--- WebPlex/Utilities/Utilities.cs
+++ WebPlex/Utilities/Utilities.cs
@@ -12,6 +12,7 @@ using System.Net;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using UnhandledExceptions;
+using System.Collections;
 
 namespace WebPlex
 {
@@ -366,6 +367,34 @@ namespace WebPlex
                     return true;
                 }
             } catch { return false; }
+        }
+
+        /// <summary>
+        /// Returns file names from given folder that comply to given filters
+        /// </summary>
+        /// <param name="SourceFolder">Folder with files to retrieve</param>
+        /// <param name="Filter">Multiple file filters separated by | character</param>
+        /// <param name="searchOption">File.IO.SearchOption, 
+        /// could be AllDirectories or TopDirectoryOnly</param>
+        /// <returns>Array of FileInfo objects that presents collection of file names that 
+        /// meet given filter</returns>
+        public static string[] getFiles(string SourceFolder, string Filter, SearchOption searchOption)
+        {
+            // ArrayList will hold all file names
+            ArrayList alFiles = new ArrayList();
+
+            // Create an array of filter string
+            string[] MultipleFilters = Filter.Split('|');
+
+            // for each filter find mathing file names
+            foreach (string FileFilter in MultipleFilters)
+            {
+                // add found file names to array list
+                alFiles.AddRange(Directory.GetFiles(SourceFolder, FileFilter, searchOption));
+            }
+
+            // returns string array of relevant file names
+            return (string[])alFiles.ToArray(typeof(string));
         }
 
         // check for updates, installs installer to downloads folder if available and opens it
