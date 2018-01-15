cfaed49a112279ff428fe7efd6a48e7f506d62a6
NV <bettercodes1@gmail.com>
2018-01-15 15:40:00 +0000

    Added Changelog, Terms of Use & Privacy Policy template form

---
+++ WebPlex/CControls/ctrlInfoTemplate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebPlex.CControls
+{
+    public partial class ctrlInfoTemplate : UserControl
+    {
+        public ctrlInfoTemplate()
+        {
+            InitializeComponent();
+        }
+
+        //protected override void OnPaint(PaintEventArgs e) { }
+
+        private void appClose_Click(object sender, EventArgs e)
+        {
+            frmWebPlex.form.frmInfo.Hide();
+        }
+
+        private void ctrlInfoTemplate_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ctrlInfoTemplate_Scroll(object sender, ScrollEventArgs e)
+        {
+            panelDataItems.Refresh();
+        }
+    }
+}
