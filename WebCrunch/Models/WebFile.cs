b460ed1a107f6fee25a8fe7e95f40375de4ba72f
NV <bettercodes1@gmail.com>
2018-01-26 11:58:27 -0900

    Attribute renames

--- WebCrunch/Models/WebFile.cs
+++ WebCrunch/Models/WebFile.cs
@@ -7,11 +7,11 @@ namespace Models
     /// </summary>
     public partial class WebFile
     {
-        public string URL { get; set; }
         public string Type { get; set; }
+        public string Name { get; set; }
         public long Size { get; set; } = 0;
         public DateTime DateUploaded { get; set; } = default(DateTime);
         public string Host { get; set; }
-        public string Name { get; set; }
+        public string URL { get; set; }
     }
 }
