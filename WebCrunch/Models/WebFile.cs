84ae42ca4a102852fd53340b9bd15b98100a7db3
NV <bettercodes1@gmail.com>
2018-01-26 10:48:21 -0900

    Add default values

--- WebCrunch/Models/WebFile.cs
+++ WebCrunch/Models/WebFile.cs
@@ -1,4 +1,6 @@
-﻿namespace Models
+﻿using System;
+
+namespace Models
 {
     /// <summary>
     /// Represents a Web File
@@ -7,8 +9,8 @@
     {
         public string URL { get; set; }
         public string Type { get; set; }
-        public string Size { get; set; }
-        public string DateAdded { get; set; }
+        public int Size { get; set; } = 0;
+        public DateTime DateUploaded { get; set; } = default(DateTime);
         public string Host { get; set; }
         public string Title { get; set; }
     }
