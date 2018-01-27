36e3096944177e69187f60f85e4b02d444b3ce16
Ashley <bettercodes1@gmail.com>
2018-01-27 22:58:55 +0000

    Update Updates.cs
    Catch check for update errors

--- WebCrunch/Utilities/Updates.cs
+++ WebCrunch/Utilities/Updates.cs
@@ -13,21 +13,30 @@ namespace WebCrunch.Utilities
         /// </summary>
         public static void checkForUpdate()
         {
-            Version newVersion = null;
-            WebClient client = new WebClient();
-            Stream stream = client.OpenRead(MainForm.linkLatestVersion);
-            StreamReader reader = new StreamReader(stream);
-            newVersion = new Version(reader.ReadToEnd());
-            Version curVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-
-            if (curVersion.CompareTo(newVersion) < 0)
+            try
             {
-                MessageBox.Show(MainForm.form, "WebCrunch " + newVersion.ToString() + " is ready to be installed.", "WebCrunch - Update Available");
+                Version newVersion = null;
+                WebClient client = new WebClient();
+                Stream stream = client.OpenRead(MainForm.linkLatestVersion);
+                StreamReader reader = new StreamReader(stream);
+                newVersion = new Version(reader.ReadToEnd());
+                Version curVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
 
-                client.DownloadFile(MainForm.getLatestInstaller(newVersion), MainForm.pathDownloadInstaller);
-                Directory.Delete(MainForm.pathData, true);
-                Process.Start(MainForm.pathDownloadInstaller);
-                Process.Start(Application.StartupPath + @"\AutoUpdater.exe");
+                if (curVersion.CompareTo(newVersion) < 0)
+                {
+                     MessageBox.Show(MainForm.form, "WebCrunch " + newVersion.ToString() + " is ready to be installed.", "WebCrunch - Update Available");
+
+                     client.DownloadFile(MainForm.getLatestInstaller(newVersion), MainForm.pathDownloadInstaller);
+                     Directory.Delete(MainForm.pathData, true);
+                     Process.Start(MainForm.pathDownloadInstaller);
+                     Process.Start(Application.StartupPath + @"\AutoUpdater.exe");
+                     Application.Exit();
+                }
+            } 
+            catch
+            {
+                MessageBox.Show("There was an error checking for update. You will be directed to the latest available version download page.");
+                Process.Start(MainForm.linkLatestDownload);
                 Application.Exit();
             }
         }
