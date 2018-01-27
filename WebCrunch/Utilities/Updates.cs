ff62cefb0f9b333bee2deacb5524cf215417df4f
NV <bettercodes1@gmail.com>
2018-01-27 22:27:02 +0000

    Update fixes

--- WebCrunch/Utilities/Updates.cs
+++ WebCrunch/Utilities/Updates.cs
@@ -8,7 +8,9 @@ namespace WebCrunch.Utilities
 {
     class Updates
     {
-        // check for updates, installs installer to downloads folder if available and opens it
+        /// <summary>
+        /// Check application for update. Installs latest installer to user downloads folder and opens it and closes this instance
+        /// </summary>
         public static void checkForUpdate()
         {
             Version newVersion = null;
@@ -18,14 +20,13 @@ namespace WebCrunch.Utilities
             newVersion = new Version(reader.ReadToEnd());
             Version curVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
 
-            //if (curVersion.CompareTo(newVersion) < 0)
-            if (curVersion.CompareTo(newVersion) > 0)
+            if (curVersion.CompareTo(newVersion) < 0)
             {
                 MessageBox.Show(MainForm.form, "WebCrunch " + newVersion.ToString() + " is ready to be installed.", "WebCrunch - Update Available");
 
-                //client.DownloadFile(MainForm.getLatestInstaller(newVersion), MainForm.pathDownloadInstaller);
-                //Directory.Delete(MainForm.pathData, true);
-                //Process.Start(MainForm.pathDownloadInstaller);
+                client.DownloadFile(MainForm.getLatestInstaller(newVersion), MainForm.pathDownloadInstaller);
+                Directory.Delete(MainForm.pathData, true);
+                Process.Start(MainForm.pathDownloadInstaller);
                 Process.Start(Application.StartupPath + @"\AutoUpdater.exe");
                 Application.Exit();
             }
