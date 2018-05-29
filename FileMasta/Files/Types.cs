51ca3f6f292797371b3f0e68abbff2ff8bda6c8a
HerbL27 <bettercodes1@gmail.com>
2018-05-29 13:32:44 +0100

    Added audio file types, re-arranged order

--- FileMasta/Files/Types.cs
+++ FileMasta/Files/Types.cs
@@ -9,11 +9,12 @@ namespace FileMasta.Files
         /// </summary>
         public static List<string> All { get; set; } = new List<string>();
         public static List<string> Video { get; } = new List<string>() { "M2TS", "MP4", "MKV", "AVI", "MPEG", "MPG", "MOV" };
-        public static List<string> Books { get; } = new List<string>() { "MOBI", "CBZ", "CBR", "CBC", "CHM", "EPUB", "FB2", "LIT", "LRF", "ODT", "PDF", "PRC", "PDB", "PML", "RB", "RTF", "TCR", "DOC", "DOCX" };
         public static List<string> Audio { get; } = new List<string>() { "MP3", "WMA", "WAV", "M3U", "APE", "AIF", "MPA", "CDA", "AC3", "OGG", "FLAC", "M4A" };
+        public static List<string> Image { get; } = new List<string>() { "TIFF", "TIF", "JPEG", "JPG", "BMP", "GIF", "PNG", "EPS", "RAW", "SVG" };
+        public static List<string> Book { get; } = new List<string>() { "MOBI", "CBZ", "CBR", "CBC", "CHM", "EPUB", "FB2", "LIT", "LRF", "ODT", "PDF", "PRC", "PDB", "PML", "RB", "RTF", "TCR", "DOC", "DOCX" };
         public static List<string> Software { get; } = new List<string>() { "EXE", "VOB", "ZIP", "TAR", "RAR", "7Z", "ISO", "PKG", "TAR.GZ", "APK", "IPA", "APPX", "XAP" };
         public static List<string> Torrent { get; } = new List<string>() { "TORRENT" };
         public static List<string> Subtitle { get; } = new List<string>() { "SRT", "SUB", "VTT" };
-        public static List<string> Other { get; } = new List<string>() { "JSP", "PL", "PHP", "HTML", "ASPX", "XML", "TXT", "SQL", "CSV" };
+        public static List<string> Other { get; } = new List<string>() { "JSP", "SWF", "PS", "RTF", "BAS", "CC", "C", "CPP", "CXX", "H", "HPP", "CS", "JAVA", "PY", "PL", "PHP", "HTML", "ASPX", "XML", "TXT", "SQL", "CSV" };
     }
 }
