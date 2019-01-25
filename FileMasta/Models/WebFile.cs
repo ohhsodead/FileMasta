a1f059c5cdd3224b828cecf443b6c9578a7283ba
Ashley Davies <imAshleyDavies@gmail.com>
2019-01-25 22:26:50 +0000

    Given appropriate object name

---
+++ FileMasta/Models/WebFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using FileMasta.Data;
+
+namespace FileMasta.Models
+{
+    /// <summary>
+    /// Represents a web file object
+    /// </summary>
+    public class WebFile
+    {
+        public readonly string Name;
+        public readonly long Size;
+        public readonly DateTime LastModified;
+        public readonly string Url;
+
+        public WebFile(string name, long size, DateTime lastModified, string url)
+        {
+            Name = name;
+            Size = size;
+            LastModified = lastModified;
+            Url = url;
+        }
+
+        public string GetExtension()
+        {
+            return Path.GetExtension(Url)?.Replace(".", "").ToUpper();
+        }
+
+        public bool IsType(string[] type)
+        {
+            return type == Types.All || type.Any(x => GetExtension().Contains(x.ToUpper()));
+        }
+    }
+}
